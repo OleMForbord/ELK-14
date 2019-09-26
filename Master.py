60ca1eddc25a217791a830a492446904074b8d4e
olemaripus <omforbor@stud.ntnu.no>
2019-09-26 18:00:08 +0200

    Minor improvements on act_pow and reac_pow

--- Master.py
+++ Master.py
@@ -1,9 +1,6 @@
-import np as np
-
 from PowerEq import *
 import numpy as np
-
-teta = np.zeros(3) #creates an array with the initial angles
+"""teta = np.zeros(3) #creates an array with the initial angles
 #v = np.array([1.0, 1.0, 1.0]) #creates an array with the initial voltages
 v = np.full((1,3), 1, dtype = float)
 
@@ -17,25 +14,46 @@ j = 0
 g = np.zeros((3,3))
 for i in range(len(y)):
     if(i == j):
-        g[i][j] = y[i].real+y[j+1].real
+        g[i][j] = y[i].real+y[j+1].real"""
+
+teta = np.array([0.0, 0.0, 0.0]) #creates an array with the initial angles
+v = np.array([1.0, 1.0, 1.0]) #creates an array with the initial voltages
+g12=(1/complex(0.05,0.2)).real
+g13=(1/complex(0.05,0.1)).real
+g23=(1/complex(0.05,0.15)).real
+b12=(1/complex(0.05,0.2)).imag
+b13=(1/complex(0.05,0.1)).imag
+b23=(1/complex(0.05,0.15)).imag
 
 
-
-#g=np.array([[g12+g13,g12,g13],[g12,g12+g23,g23],[g13,g23,g13+g23]]) #creates a matrix with the conductanses
-#b=np.array([[b12+b13,b12,b13],[b12,b12+b23,b23],[b13,b23,b13+b23]]) #creates a matrix with the susceptanses
+g=np.array([[g12+g13,g12,g13],[g12,g12+g23,g23],[g13,g23,g13+g23]]) #creates a matrix with the conductanses
+b=np.array([[b12+b13,b12,b13],[b12,b12+b23,b23],[b13,b23,b13+b23]]) #creates a matrix with the susceptanses
 Pindex=np.array([1,2])#list with bus number of buses with known P
 Qindex=np.array([1,2])#list with bus number of buses with known Q
 vindex=np.array([1,2])#list with bus number of buses with unknown v
 tindex=np.array([1,2])#list with bus number of buses with unknown angel
-Pactual=np.array([-1.0,-0.5])
+Pactual=np.array([-0.8,-0.4])
 Qactual=np.array([-0.5,-0.5])
 
-print(jacobi(Pindex,Qindex,tindex,vindex,v,teta,g,b))
+#print(jacobi(Pindex,Qindex,tindex,vindex,v,teta,g,b))
 
-print(power_missmatch(Pactual,Qactual,Pindex,Qindex,v,teta,g,b))
-#newtonrhapson(Pactual,Qactual,Pindex,Qindex,tindex,vindex,v,teta,g,b)
+#print(power_missmatch(Pactual,Qactual,Pindex,Qindex,v,teta,g,b))
 
+CONVERGENCE_LIMIT = 10**3
+counter = 0
+#flag = True
 
+while True:
 
+    if (newtonrhapson(Pactual,Qactual,Pindex,Qindex,tindex,vindex,v,teta,g,b) == 0):
+        break
+        #flag = False
 
+    Pactual = Pactual - [0.2 * 0.3, 0.2 * 0.7]
+    Qactual = Qactual - [0.2 * 0.3, 0.2 * 0.7]
+
+print("Divergence at Pactual: ", Pactual)
+plt.xlabel("Load power")
+plt.ylabel("Voltage")
+plt.show()
 
--- PowerEq.py
+++ PowerEq.py
@@ -1,5 +1,6 @@
 from math import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 def tij(
     i, j, teta, g, b
@@ -23,18 +24,28 @@ def act_pow(busi, n, v, teta, g, b):  # n=number of buses
         else:
             p = p - v[busi-1] * v[busj-1] * tij(busi, busj, teta, g, b)
     return p
+    """pi = 0
+    for busj in range(1, n+1):
+
+        pi += (v[busi-1] * v[busj-1] * tij(busi, busj, teta, g,b))
+
+    return pi"""
+
 
 
 def react_pow(busi, n, v, teta, g, b):  # n=number of buses
-    q = -(v[busi-1]**2) * b[busi-1][busi-1]
-    for busj in range(1, n+1):
-        if busj == busi:
-            continue
-        else:
-            print('it did really continue')
-            q = q - v[busi-1] * v[busj-1] * uij(busi, busj, teta, g, b)
-    return q
+   q = -(v[busi-1]**2) * b[busi-1][busi-1]
+   for busj in range(1, n + 1):
+       if busj == busi:
+           continue
+       else:
+           q = q - v[busi-1] * v[busj-1] * uij(busi, busj, teta, g, b)
+   return q
 
+   """qi = 0
+   for busj in range(1, n + 1):
+        qi += (v[busi-1] * v[busj-1] * uij(busi, busj, teta, g, b))
+   return qi"""
 
 def dpdt(
     busi, tindex, v, teta, g, b
@@ -107,7 +118,7 @@ def dqdv(busi, vindex, v, teta, g, b):
 
 
 def jacobi(Pindex, Qindex, tindex, vindex, v, teta, g, b):
-    jac = np.empty(shape=(Pindex.size + Qindex.size, tindex.size + vindex.size))
+    jac = np.empty(shape=(Pindex.size+ Qindex.size, tindex.size + vindex.size))
     for pi in range(0,Pindex.size):
         row = np.empty(0)
         row = np.append(row, dpdt(Pindex[pi], tindex, v, teta, g, b))
@@ -122,29 +133,51 @@ def jacobi(Pindex, Qindex, tindex, vindex, v, teta, g, b):
 
 def power_missmatch(Pactual,Qactual,Pindex,Qindex,v,teta,g,b):
     actualpower=np.append(Pactual,Qactual)
-    print(actualpower)
+    #print(actualpower)
     estpower=np.empty(0)
     for pi in range(0,Pindex.size):
         estpower=np.append(estpower,act_pow(Pindex[pi],v.size,v,teta,g,b))
     for qi in range(0,Qindex.size):
         estpower=np.append(estpower,react_pow(Qindex[qi],v.size,v,teta,g,b))
-    print(estpower)
+    #print(estpower)
     return actualpower-estpower
 
 def newtonrhapson(Pactual,Qactual,Pindex, Qindex, tindex, vindex, v, teta, g, b):
-    jacobiinv=np.linalg.inv(jacobi(Pindex,Qindex,tindex,vindex,v,teta,g,b))
-    deltapower= power_missmatch(Pactual,Qactual,Pindex,Qindex,vindex,v,teta,g,b)
-    correction=jacobiinv.dot(deltapower)
-    print('Jacobi-matrix:\n',jacobi(Pindex,Qindex,tindex,vindex,v,teta,g,b))
-    print('\nVoltage angles:')
-    for a in range(0,tindex.size):
-        teta[a]=teta[a]+correction[a]
-        print('Bus nr',tindex[a],': Voltage angle =',teta[a] )
-    print('\nVoltage magnetudes:')
-    for vm in range(0,vindex.size):
-        v[vm-1]=v[vm]+correction[tindex.size-1+vm]
-        print('Bus nr',vindex[vm],': Voltage magnitude =',v[vm])
+    it = 1
+    epsilonError = 0.001
+    deltapower = power_missmatch(Pactual, Qactual, Pindex, Qindex, v, teta, g, b)
+    jacobiinv = np.linalg.inv(jacobi(Pindex, Qindex, tindex, vindex, v, teta, g, b))
+    correction = jacobiinv.dot(deltapower)
+    from Master import CONVERGENCE_LIMIT
+    #while abs(correction[2]) > epsilonError:
+    while abs(correction[2]) > epsilonError:
+        if(it >= CONVERGENCE_LIMIT):
+
+            return 0
+
+        #print('Jacobi-matrix:\n', jacobi(Pindex, Qindex, tindex, vindex, v, teta, g, b))
+        i = 0
+        #print("\nCorection: \n", correction)
+        for a in range(0, tindex.size):
+            teta[a] = teta[a] + correction[a]
+            #print('Bus nr', tindex[a], ': Voltage angle =', teta[a])
+
+        for vm in range(0, vindex.size):
+
+            v[vm] = v[vm] + correction[tindex.size - 1 + vm]
+            #print('Bus nr', vindex[vm], ': Voltage magnitude =', v[vm])
+        deltapower = power_missmatch(Pactual, Qactual, Pindex, Qindex, v, teta, g, b)
+        jacobiinv = np.linalg.inv(jacobi(Pindex, Qindex, tindex, vindex, v, teta, g, b))
+        correction = jacobiinv.dot(deltapower)
+        it += 1
+    print("Number of iterations before convergence is: ", it)
+    print('Bus nr', vindex[0], ': Voltage magnitude =', v[0])
+    print('Bus nr ', vindex[1], ': Voltage magnitude =', v[1])
+
+    plt.plot(-sum(Pactual), v[0], 'g^-', -sum(Pactual), v[1], 'bo-')
+
+
+    return 1
     #for i in correction:
        # while i>0.5:
         #    newtonrhapson(Pactual,Qactual,Pindex,Qindex,tindex,vindex,v,teta,g,b)
-    #return 0
