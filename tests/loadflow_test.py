7478419eb71457439ef8db23915b73b35fae1c2d
Åsmund Sælen <aa.selen@hotmail.com>
2019-10-14 19:57:16 +0200

    FDLF added to the power system methods

--- tests/loadflow_test.py
+++ tests/loadflow_test.py
@@ -1,4 +1,5 @@
 from Loadflow.PowerEq import *
+from Decoupled.FDLF import *
 import numpy as np
 """teta = np.zeros(3) #creates an array with the initial angles
 #v = np.array([1.0, 1.0, 1.0]) #creates an array with the initial voltages
@@ -18,16 +19,14 @@ for i in range(len(y)):
 """
 teta = np.array([0.0, 0.0, 0.0]) #creates an array with the initial angles
 v = np.array([1.0, 1.0, 1.0]) #creates an array with the initial voltages
-g12=(1/complex(0.05,0.2)).real
-g13=(1/complex(0.05,0.1)).real
-g23=(1/complex(0.05,0.15)).real
-b12=(1/complex(0.05,0.2)).imag
-b13=(1/complex(0.05,0.1)).imag
-b23=(1/complex(0.05,0.15)).imag
+z12=(complex(0.05,0.2))
+z13=complex(0.05,0.1)
+z23=complex(0.05,0.15)
 
+z=np.array([[0,z12,z13],[z12,0,z23],[z13,z23,0]])
+g=g_matrix(z) #creates a matrix with the conductanses
+b=b_matrix(z) #creates a matrix with the susceptances
 
-g=np.array([[g12+g13,g12,g13],[g12,g12+g23,g23],[g13,g23,g13+g23]]) #creates a matrix with the conductanses
-b=np.array([[b12+b13,b12,b13],[b12,b12+b23,b23],[b13,b23,b13+b23]]) #creates a matrix with the susceptanses
 Pindex=np.array([1,2])#list with bus-numbers of the buses with known P
 Qindex=np.array([1,2])#list with bus-numbers of the buses with known Q
 vindex=np.array([1,2])#list with bus-numbers of the buses with unknown v
@@ -38,8 +37,12 @@ alpha = np.array([0.1,0.2])
 beta = np.array([1,2])
 
 
+print_fdlf(Pactual,Qactual,Pindex, Qindex, tindex, vindex, v, teta, g, b,z)
+#print_primal(Pactual,Qactual,Pindex, Qindex, tindex, vindex, v, teta, g, b,z)
+#print(h_matrix(Pindex,tindex,v,teta,g,b))
+
 #newtonrhapson_print(Pactual,Qactual,Pindex,Qindex,tindex,vindex,v,teta,g,b)
-voltageStabilityFlatStart(Pactual,Qactual,Pindex, Qindex, tindex, vindex, g, b)
+#voltageStabilityFlatStart(Pactual,Qactual,Pindex, Qindex, tindex, vindex, g, b)
 #voltageStabilityFlatStart(Pactual,Qactual,Pindex, Qindex, tindex, vindex, g, b)
 
 
